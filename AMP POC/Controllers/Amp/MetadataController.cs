8824572e6301c96585f7d67b67263dcc0595c8cd
raigunn <daniel@velir.com>
2017-11-24 12:36:15 -0500

    Css Rendering is working with sample css file now

---
+++ AMP POC/Controllers/Amp/MetadataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AMP_POC.Controllers.Amp
+{
+    public class MetadataController : Controller
+    {
+        // GET: Metadata
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AmpCssControllerRendering()
+        {
+            string css = System.IO.File.ReadAllText(Server.MapPath("~/styles/sample.css"));
+            return View(new MvcHtmlString(css));// return View(css);
+        }
+    }
+}
